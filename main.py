2fe0f10eec8b95ab4ef9fd7f37f513b44d6a2cba
jigneshsapra1990 <jignesh.sapra@gmail.com>
2026-05-13 21:24:11 +0530

    Start working on query param

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/{id}")
-async def root(id:int):
+def root(id:int):
     return {"message": f"Hello Fast Api {id}"}
+
+@app.post("/todo")
+def addPost(item: dict):
+    return {"message": f"Hello Fast Api {item}"}
